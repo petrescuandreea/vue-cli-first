1e8961fbe1f3d148a678f22bf7122a3cad19f2b5
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-23 16:13:57 +0100

    creato componente hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    div {
+        height: 400px;
+        background-image: url('../assets/bg-hero.jpg');
+        background-size: contain;
+        background-position: top;
+        background-repeat: no-repeat;
+    }
+</style>
