fec8c1c0cdcd1d95226b426b2957a9d3dede7bdc
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-23 15:31:16 +0100

    creato componente Title

---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
